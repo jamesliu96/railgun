5311cc66340a8647ca1aab4530cc24a5376e8968
James Liu <jamesliu96@users.noreply.github.com>
2021-03-05 15:18:59 +0800

    refactor: reduce for better documentation

--- reduce.ts
+++ reduce.ts
@@ -3,21 +3,20 @@ import { Context } from './context.ts';
 
 export type ReducedMiddleware = (ctx: Context, next?: Next) => Empty;
 
-export const reduce = (middlewares: Middleware[]): ReducedMiddleware => (
-  ctx,
-  next
-) => {
-  let idx = -1;
-  const dispatch = async (i: number) => {
-    if (i <= idx) throw new Error('next() called multiple times');
-    idx = i;
-    const fn = i === middlewares.length ? next : middlewares[i];
-    if (!fn) return;
-    try {
-      await fn(ctx, dispatch.bind(null, i + 1));
-    } catch (e) {
-      throw e;
-    }
+export function reduce(middlewares: Middleware[]): ReducedMiddleware {
+  return (ctx, next) => {
+    let idx = -1;
+    const dispatch = async (i: number) => {
+      if (i <= idx) throw new Error('next() called multiple times');
+      idx = i;
+      const fn = i === middlewares.length ? next : middlewares[i];
+      if (!fn) return;
+      try {
+        await fn(ctx, dispatch.bind(null, i + 1));
+      } catch (e) {
+        throw e;
+      }
+    };
+    return dispatch(0);
   };
-  return dispatch(0);
-};
+}
