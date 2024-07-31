7b3b59c1936bb712b8d4e1af4ea1003806901d61
James Liu <jamesliu96@users.noreply.github.com>
2024-07-31 15:01:24 +0800

    build: upgrade deps

--- deps.ts
+++ deps.ts
@@ -3,14 +3,14 @@ export {
   getSetCookies,
   setCookie,
   deleteCookie,
-} from 'https://deno.land/std@0.223.0/http/mod.ts';
-export type { Cookie } from 'https://deno.land/std@0.223.0/http/mod.ts';
+} from 'https://deno.land/std@0.224.0/http/mod.ts';
+export type { Cookie } from 'https://deno.land/std@0.224.0/http/mod.ts';
 export {
   STATUS_CODE,
   STATUS_TEXT,
-} from 'https://deno.land/std@0.223.0/http/status.ts';
+} from 'https://deno.land/std@0.224.0/http/status.ts';
 export type {
   StatusCode,
   StatusText,
-} from 'https://deno.land/std@0.223.0/http/status.ts';
-export { contentType } from 'https://deno.land/std@0.223.0/media_types/mod.ts';
+} from 'https://deno.land/std@0.224.0/http/status.ts';
+export { contentType } from 'https://deno.land/std@0.224.0/media_types/mod.ts';
